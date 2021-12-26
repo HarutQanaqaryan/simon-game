174f63b8af8d9d6cd391b5919a9fdb41606d9ff5
HarutQanaqaryan <hqanaqaryan@gmail.com>
2021-12-26 11:01:39 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+ <Simon />
+</template>
+<script>
+import Simon from "./components/Simon.vue";
+
+export default {
+    components: { Simon }
+}
+</script>
+<style lang="scss">
+@import "./assets/simon.scss";
+</style>
---
+++ src/components/Simon.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="simon-wrapper">
+    <div class="simon-game">
+      <div class="simon-left-side">
+        <div class="simon-left-top"></div>
+        <div class="simon-left-bottom"></div>
+      </div>
+      <div class="simon-center">
+        <div class="simon-center_min">
+          <h2 class="simon-title">SIMON</h2>
+          <div class="simon-levels">
+            <span>
+              <p class="simon-level-title">Легкий</p>
+              <button class="simon-level_btn"></button>
+            </span>
+            <span>
+              <p class="simon-level-title">Средний</p>
+              <button class="simon-level_btn"></button>
+            </span>
+            <span>
+              <p class="simon-level-title">Сложный</p>
+              <button class="simon-level_btn"></button>
+            </span>
+          </div>
+          <span class="simon-start-points">
+            <span class="simon-start">Старт</span>
+            <span class="simon-steps">0</span>
+          </span>
+        </div>
+      </div>
+      <div class="simon-right-side">
+        <div class="simon-right-top"></div>
+        <div class="simon-right-bottom"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
